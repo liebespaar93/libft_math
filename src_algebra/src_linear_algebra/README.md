2381a93d8f5278db8c4c4563a35d7e028e85a659
MacBook Air <liebespaar93@gmail.com>
2022-07-29 09:35:24 +0900

    2022/07/29 dimension modules

---
+++ src_algebra/src_linear_algebra/README.md
@@ -0,0 +1,9 @@
+# linear_algebra
+선형 대수학에 대한 정보다
+
+3brown 1blue 썬형대수학에 대한 설명이다
+유투브 :: https://www.youtube.com/watch?v=fNk_zzaMoSs&list=PLZHQObOWTQDPD3MizzM2xVFitgF8hE_ab
+
+코드 구현한 부분의 영상이다
+유투브 :: https://www.youtube.com/watch?v=io-UmoAXVWk&list=PLrrTotxaO6khHInVhLSw3X16VucWW1v1Y
+
---
+++ src_dimension/src/ft_memset.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kyoulee <kyoulee@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/28 16:57:00 by kyoulee           #+#    #+#             */
+/*   Updated: 2022/07/28 16:57:01 by kyoulee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ft_dimension.h>
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char	*ptr;
+
+	ptr = (unsigned char *)b;
+	while (len-- > 0)
+		*ptr++ = c;
+	return (b);
+}
